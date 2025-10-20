47d5b7f4fb69736f98617acbc0e069c192abc74a
K W A <khaledwalidabass@gmail.com>
2025-10-20 15:21:39 -0700

    fdgdfgdr

--- mian.js
+++ mian.js
@@ -17,5 +17,23 @@ element.css("width", `${(top / hie) * 100}%`)
     $(".UP").click(function () {
         $("html, body").animate({ scrollTop: 0 }, 1000);
     })
-    
+})
+// shuffle
+let arrLi = Array.from(document.querySelectorAll("ul.shuffle li"))
+let arrBox = Array.from(document.querySelectorAll(".imgs-container .box"))
+
+arrLi.forEach(ele => {
+    ele.addEventListener("click", function (e) {
+        arrLi.forEach(el => {
+            el.classList.remove("active")
+        })
+        e.currentTarget.classList.add("active")
+           arrBox.forEach(el => {
+            el.style.display ="none"
+           })
+        document.querySelectorAll(e.currentTarget.dataset.type).forEach(element => {
+            element.style.display = "block"
+        })
+            // .style.display = "block"
+    })
 })
